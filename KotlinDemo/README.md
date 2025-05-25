eafccf8a6deed40ccbae53a9c8278666b30fef39
LeoAndo <leo.777.info@gmail.com>
2025-05-25 13:10:54 +0900

    udpate

---
+++ KotlinDemo/README.md
@@ -0,0 +1,8 @@
+# Android Studio
+
+Android Studio Narwhal Feature Drop | 2025.1.2 Nightly 2025-05-23
+
+# capture Pixel 9 Pro XL API 36 (OS 16)
+| 1                              | 2                              | 3                              |
+--------------------------------|--------------------------------|-------------------------------- 
+| <img src="./1.png" width=50%/> | <img src="./2.png" width=50%/> | <img src="./3.png" width=50%/> |
---
+++ KotlinDemo/app/src/main/java/jp/ac/jec/cm0199/kotlindemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package jp.ac.jec.cm0199.kotlindemo
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<View>(R.id.button).setOnClickListener { view: View ->
+            val url = "https://developers.android.com"
+            val intent = CustomTabsIntent.Builder().build()
+            intent.launchUrl(this, url.toUri())
+        }
+    }
+}
