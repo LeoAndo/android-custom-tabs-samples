5c49f9c6a2ea10133811475bff54c0f7ee726cf3
LeoAndo <leo.777.info@gmail.com>
2025-05-25 13:06:01 +0900

    update

---
+++ JavaDemo/app/src/main/java/jp/ac/jec/cm0199/javademo/MainActivity.java
@@ -0,0 +1,34 @@
+package jp.ac.jec.cm0199.javademo;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.button).setOnClickListener(view -> {
+            var url = "https://developers.android.com";
+            var intent = new CustomTabsIntent.Builder().build();
+            intent.launchUrl(this, Uri.parse(url));
+        });
+    }
+
+}
